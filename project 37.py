86a5633cdf6672b16b7c0bde92bdce67e023c010
Maryam Abdulhakim <maryamabdulhakim2010@gmail.com>
2025-10-17 11:12:49 +0300

    project 37

---
+++ project 37.py
@@ -0,0 +1,13 @@
+Total = 0
+for i in range(1, 10):
+    Total += i
+print(f"Total sum is: {Total}")
+
+print(1+2)
+print(1+2+3)
+print(1+2+3+4)
+print(1+2+3+4+5)
+print(1+2+3+4+5+6)
+print(1+2+3+4+5+6+7)
+print(1+2+3+4+5+6+7+8)
+print(1+2+3+4+5+6+7+8+9)
