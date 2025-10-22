c4c537821e7185db98687aefe1ea246f75efd762
maryamcody <maryamabdulhakim2010@gmail.com>
2025-10-22 10:29:02 +0300

    Add files via upload

---
+++ project37-2.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+num_str=str(num)
+power = len(num_str)
+total =sum(int(digit) ** power for digit in num_str)
+if total == num:
+    print(f"{num} is an Armstrong number.")
+else:
+    print(f"{num} is not an Armstrong number.")
